8d85766c70c28aec414b5d5b6fc6ef87ef30f92c
44 <94141577+9xN@users.noreply.github.com>
2023-11-06 22:40:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ const generateReadme = (vpnList) => {
     
     content += `## Available Servers\n\n`;
     content += `Below is the list of available VPN servers:\n\n`;
-    content += `(You may connect to any of these VPN servers with: Username: 'vpn', Password: 'vpn'.`
+    content += `(You may connect to any of these VPN servers with: Username: 'vpn', Password: 'vpn'.\n`;
     content += "| Hostname | IP Address | Ping | Speed | Country | OpenVPN Config | Score |\n";
     content += "|----------|------------|-------|-------|---------|----------------| ----- |\n";
     vpnList.servers.forEach((server, index) => {
         let speedInMbps = (server.speed / 10000000).toFixed(2); // Convert to Mbps and round to two decimal places
-        content += `| ${server.hostname} | ${server.ip} | ${server.ping} | ${speedInMbps} Mbps | ${server.countrylong} | [Download 📥](./configs/server_${index}_${server.countryshort}.ovpn) | ${server.score}\n`;
+        content += `| ${server.hostname} | ${server.ip} | ${server.ping} | ${speedInMbps} Mbps | ${server.countrylong} | [Download 📥](./configs/server_${index}_${server.countryshort}.ovpn) | ${server.score} |\n`;
     });
     fs.writeFileSync('README.md', content);
 }
