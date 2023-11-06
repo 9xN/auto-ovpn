c2b67807d65b73417d5605f02db7131733779c76
44 <94141577+9xN@users.noreply.github.com>
2023-11-06 22:45:33 +0000

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const generateReadme = (vpnList) => {
     content += "|----------|------------|-------|-------|---------|----------------| ----- |\n";
     vpnList.servers.forEach((server, index) => {
         let speedInMbps = (server.speed / 10000000).toFixed(2); // Convert to Mbps and round to two decimal places
-        content += `| ${server.hostname} | ${server.ip} | ${server.ping} | ${speedInMbps} Mbps | ${server.countrylong} | [Download 📥](./configs/server_${index}_${server.countryshort}.ovpn) | ${server.score} |\n`;
+        content += `| ${server.hostname} | ${server.ip} | ${server.ping} | ${speedInMbps} Mbps | ${server.countrylong} | [Download 📥](./configs/server_${index}_${server.countryshort}.ovpn) | ${server.score / 100000} |\n`;
     });
     fs.writeFileSync('README.md', content);
 }
